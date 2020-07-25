df61b482723f3c1a17ce048bbf83c32a173118f5
Michael LaRoy <mike.laroy@gmail.com>
2020-07-25 14:16:08 -0700

    add corporate component

---
+++ src/components/instagramCorporate.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby'
+import { Link } from 'gatsby'
+
+const Instagram = props => (
+  <StaticQuery
+    query={INSTA_QUERY}
+    render={({ allInstaNode }) => (
+      <>
+        <section className="bg-white" id="corporate">
+          <div className="container">
+            <div className="lg:w-3/4">
+              <h2>Corporate</h2>
+              <p>
+                Corporations or small business owners are welcome to submit imagery or a detailed concept of what they would like captured through watercolour sketching. In addition to the original, replicates of the painting in print material or digital format are available for a licensing fee.
+                Below are a few corporate customers that Katrina Dawn has worked with.
+              </p>
+              <Link to="/pricing" className="link">See my prices</Link>
+            </div>
+            <div id="instafeed" className="instafeed is-visible">
+              {allInstaNode.edges.map(edge => {
+                return (
+                  <a href={edge.node.link} key={edge.node.id}>
+                    <img
+                      alt={`${edge.node.caption.substring(0, 100)}...`}
+                      src={edge.node.original}
+                      />
+                  </a>
+                )
+              })}
+            </div>
+          </div>
+        </section>
+      </>
+    )}
+  />
+);
+
+const INSTA_QUERY = graphql`
+  query corporateQuery {
+    allInstaNode(
+      limit: 4
+      filter: {
+        caption: {
+          regex: "/#corporateart/"
+        }
+      }
+      sort: {
+        fields: timestamp
+        order: DESC
+      }
+    ) {
+      edges {
+        node {
+          id
+          caption
+          preview
+          original
+          mediaType
+          internal {
+            type
+            contentDigest
+            owner
+          }
+        }
+      }
+    }
+  }
+`
+
+export default Instagram;
