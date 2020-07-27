1afc502897225d7acca455fce8524b2c61493737
Michael LaRoy <mike.laroy@gmail.com>
2020-07-27 16:56:46 -0700

    finish update purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,13 +59,8 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        whitelist: [
-          '.bg-color',
-          '.instafeed .error',
-          '.instafeed.is-hidden',
-          '.testimonials .slick-dots li button:before',
-          '.testimonials .slick-dots li.slick-active button:before'
-        ], // Don't remove this selector
+        whitelistPatterns: [/^slick/],
+        whitelistPatternsChildren: [/^slick/],
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       }
