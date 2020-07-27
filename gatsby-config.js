579c8165645ff0415132941ae4c129d22a59c8b2
Michael LaRoy <mike.laroy@gmail.com>
2020-07-26 17:16:25 -0700

    add tailwind whitelist

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,13 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
+        whitelist: [
+          '.bg-color',
+          '.instafeed .error',
+          '.instafeed.is-hidden',
+          '.testimonials .slick-dots li button:before',
+          '.testimonials .slick-dots li.slick-active button:before'
+        ], // Don't remove this selector
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       }
