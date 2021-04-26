488495e09f35c6ff4b81bd6ef2a14bf3a4fdb2d0
Michael LaRoy <mike.laroy@gmail.com>
2021-04-25 20:31:08 -0700

    update home portraits copy

--- src/components/contentfulHome.js
+++ src/components/contentfulHome.js
@@ -13,10 +13,8 @@ const ContentfulHome = props => (
               <h2>Home Portraits</h2>
                 <p>
                   Home portraits capture years of life spent with loved ones. They capture dreams realized.
-                  Clients submit photos of a home to me by email. Multiple photos taken from multiple angles provide a range of reference. Capturing the surrounding context – the big tree in the front yard, the fence lining the property, the adjacent telephone pole – it all adds context. Provide more context than less, and through discussion we can settle on what to include in the painting.
-                </p>
-                <p>Don’t have any photos? Not a problem. I’ve painted a number of home portraits using Google Street View for clients unable to locate pictures of a childhood home.
-                  The recommended minimum size for a home portrait is 8x10.
+                  They depict journey. Here is a sample of home portraits I’ve painted.
+                  Click an image to read the related blog post.
                 </p>
             </div>
             <div id="instafeed" className="instafeed is-visible mt-8">
