2e56a25acad590124c375dd159b3f82bcc8bb230
Michael LaRoy <mike.laroy@gmail.com>
2023-02-26 19:50:29 -0800

    add shop link to top nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,18 +5,19 @@ class Nav extends Component {
   render() {
     return (
       <div className="container flex justify-between">
-        <div className="py-4">
-          <Link to="/" className="block">
+        <div className="py-4 flex-shrink-0">
+          <Link to="/" className="block w-96">
             <img src={logo} width="125" alt="Katrina Dawn Studio Logo" />
           </Link>
         </div>
 
-        <nav className="py-4 pr-4">
+        <nav className="py-4 pr-4 nav-links flex flex-wrap lg:flex-nowrap justify-end items-center gap-2">
           <Link to="/about" className="nav-link hover:underline focus:underline">About</Link>
           <Link to="/work" className="nav-link hover:underline focus:underline">Work</Link>
           <Link to="/pricing" className="nav-link hover:underline focus:underline">Pricing</Link>
           <Link to="/blog" className="nav-link hover:underline focus:underline">Blog</Link>
           <Link to="/contact" className="nav-link hover:underline focus:underline">Contact</Link>
+          <Link to="https://www.etsy.com/shop/KatrinaDawnStudio" className="btn btn-inverse">Shop</Link>
         </nav>
       </div>
     );
